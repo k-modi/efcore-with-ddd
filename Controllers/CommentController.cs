1c804e38c2feb93ec3b6a125dad8c9a249d7eea8
Kunjan Modi <kunjanmodi@Kunjans-MacBook-Pro.local>
2017-02-07 20:43:53 -0800

    remove test commit

--- Controllers/CommentController.cs
+++ Controllers/CommentController.cs
@@ -8,7 +8,6 @@ namespace efcore2_webapi.Webapp.Controllers
     [Route("/api/[controller]")]
     public class CommentController : Controller
     {
-        //test comment
         private readonly ICommentRepository _commentRepository;
 
         public CommentController(ICommentRepository commentRepository)
